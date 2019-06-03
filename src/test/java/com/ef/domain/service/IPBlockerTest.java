ecbf2f44b488ab0ec9d0d70863402b58b89593dd
Ahmed RIAHI <ahmed.riahi@talan.com>
2019-06-03 15:07:42 +0100

    IPBlockerTest

---
+++ src/test/java/com/ef/domain/service/IPBlockerTest.java
@@ -0,0 +1,33 @@
+package com.ef.domain.service;
+
+import com.ef.persistence.repository.BlockedIPRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IPBlockerTest {
+
+    @Autowired
+    private IPBlocker ipBlocker;
+
+    @Autowired
+    private BlockedIPRepository blockedIPRepository;
+
+    @Test
+    public void given_list_of_suspicious_ips_when_calling_blocker_ip_should_be_persisted_on_database(){
+        int initialSize = this.blockedIPRepository.findAll().size();
+        this.ipBlocker.block(Arrays.asList("1.1.1.1","1.1.1.2","1.1.1.3"));
+        int afterSize = this.blockedIPRepository.findAll().size();
+        Assertions.assertThat(afterSize - initialSize).isEqualTo(3);
+    }
+
+}
